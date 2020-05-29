70df49eb12dc957dbbfa7d85d9e370d55ba606d4
Sunc4127 <42020175+Sunc4127@users.noreply.github.com>
2020-05-29 22:42:46 +1000

    upload the project file

---
+++ Whist/whist/BrokeRuleException.java
@@ -0,0 +1,9 @@
+/**
+ * An exception thrown when a player breaks a rule
+ */
+@SuppressWarnings("serial")
+public class BrokeRuleException extends Exception {
+	public BrokeRuleException(String violation) {
+		super(violation);
+	}
+}
