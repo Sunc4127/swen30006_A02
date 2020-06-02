ce5d8e496badad51f991b787a3aca1202907b852
Roy Wu <56463066+royrx93@users.noreply.github.com>
2020-06-02 22:15:16 +1000

    Delete SmartComponent.java

--- Whist/whist/strategies/SmartComponent.java
+++
@@ -1,9 +0,0 @@
-package strategies;
-
-import ch.aplu.jcardgame.Card;
-
-import java.util.ArrayList;
-
-public abstract class SmartComponent {
-    public abstract Card selectCard(ArrayList<Card> cardList) throws NoTrumpCardOnTableException;
-}
